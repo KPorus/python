598225441e2413580eb562d1753d48958f683021
KPorus <designerfardin2021@gmail.com>
2023-04-25 22:27:45 +0600

    edit in firstApp

--- FirstApp.py
+++ FirstApp.py
@@ -11,6 +11,20 @@ print(sum(x,y))
 
 print("hello world",z)
 
+#string
 firstName = "fardin"
 lastName = "khan"
 print(firstName,lastName)
+
+statement = "Fardin khan is a node js developer"
+#find is in statement using find function which return first index number of seacrhing value
+print(statement.find("is"))
+
+# finding "is" in statement which return bool
+print("is" in statement)
+
+#capitalize() function used to capitalize the string
+print(statement.capitalize())
+
+#split() to split the string in array
+print(statement.split(" "))
