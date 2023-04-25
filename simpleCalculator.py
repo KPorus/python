0190a29a2a3e04a46a71162db377625742844044
KPorus <designerfardin2021@gmail.com>
2023-04-25 22:12:50 +0600

    calculator

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,2 +1,9 @@
-q = input("what do you want (sum, multipilication, add, division) than enter(+,-,*,/) ");
-print(q)
+#q = input("what do you want (sum, multipilication, add, division) than enter(+,-,*,/) ");
+#print(q)
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter a number: "))
+
+def add(x,y):
+    return x + y
+print("result of addition: ",add(num1,num2))
