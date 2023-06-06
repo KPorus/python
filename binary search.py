9d2019156134ea6438f3d98c77d7d6fad2d5b79d
KPorus <designerfardin2021@gmail.com>
2023-06-06 19:57:47 +0600

    binary search update

--- binary search.py
+++ binary search.py
@@ -1,13 +1,11 @@
-from math import ceil
-
-data = [1,2,3,4,5,6,7,8,9]
+data = [13, 11, 10, 7, 4, 3, 1, 0]
 
 def binary_search(data, key):
     start = 0
     end = len(data) - 1
-    if len(data)>0:
+    if len(data) > 0:
         while start <= end:
-            mid = ceil((start + end) / 2)
+            mid = (start + end) // 2
 
             if data[mid] < key:
                 start = mid + 1
@@ -15,9 +13,8 @@ def binary_search(data, key):
                 end = mid - 1
             else:
                 return mid
-
         return -1
     return -1
 
-result = binary_search(data, 12)
+result = binary_search(data, 7)
 print(result)
