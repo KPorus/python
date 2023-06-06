9809c4f23891fafc8a6522a2fefa899329833c15
KPorus <designerfardin2021@gmail.com>
2023-06-06 14:13:15 +0600

    binary search

---
+++ binary search.py
@@ -0,0 +1,22 @@
+from math import ceil
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def binary_search(data, key):
+    start = 0
+    end = len(data) - 1
+
+    while start <= end:
+        mid = ceil((start + end) / 2)
+
+        if data[mid] < key:
+            start = mid + 1
+        elif data[mid] > key:
+            end = mid - 1
+        else:
+            return mid
+
+    return -1
+
+result = binary_search(data, 2)
+print(result)
