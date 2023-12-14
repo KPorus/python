297113d3a5f65188d5aefee3d5e95f6d28721760
KPorus <designerfardin2021@gmail.com>
2023-12-14 17:42:11 +0600

    add new code

--- LEARNING/function&gettingHelp.py
+++ LEARNING/function&gettingHelp.py
@@ -1,2 +1,11 @@
 help(range)
-help()
+
+def greating(who="fardin"):
+    """
+    It a greating function. which print a greating. and take a params. By default it doesn't take params.
+    """
+    print("Hello, " + who)
+
+greating(who="anika")
+greating()
+help(greating)
