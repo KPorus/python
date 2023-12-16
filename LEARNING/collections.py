10bb716bb717b457b88f4c23a52863d95cfd6ffd
KPorus <designerfardin2021@gmail.com>
2023-12-16 13:06:53 +0600

    collections

--- LEARNING/collections.py
+++ LEARNING/collections.py
@@ -6,4 +6,21 @@ import collections
 string = 'Fardin is back'
 counter = collections.Counter(string)
 print(counter.items())
-print(counter.most_common(3))
+print(list(counter.elements()))
+print(counter.most_common(3)[0][0])
+
+
+number = collections.deque()
+
+number.append(1)
+number.append(2)
+number.append(3)
+number.append(4)
+print(number)
+
+number.extend([5,6,7])
+number.extendleft([5,6,7])
+number.appendleft(10)
+print(number)
+number.rotate(-5)
+print(number)
